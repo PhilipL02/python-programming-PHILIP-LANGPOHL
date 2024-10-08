30ced0b4f832888f72268f7a7b72cf76be223e93
Philip Langpohl <philip.langpohl@outlook.com>
2024-10-08 14:06:43 +0200

    feat(lab3): plot line and classified data points

--- Labs/Lab3/lab3.py
+++ Labs/Lab3/lab3.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Constants
+PATH_TO_DATA_FOLDER = "Labs/Lab3/Data"
+
 def calculate_y_on_separator(x: float) -> float:
     # The line equation developed in the preparatory assignment, to seperate points evenly
     # Returns the y-value on the separating line for a given x-value.
@@ -12,10 +18,8 @@ def get_classification_label_for_point(point: tuple) -> int:
 
 
 def classify_unlabelled_data() -> None:
-    path_to_data_folder = "Labs/Lab3/Data"
-
     # Open both the file with unlabelled data, and the new file that will contain labelled data
-    with open(f"{path_to_data_folder}/unlabelled_data.csv", "r") as file_read, open(f"{path_to_data_folder}/labelled_data.csv", "w") as file_write:
+    with open(f"{PATH_TO_DATA_FOLDER}/unlabelled_data.csv", "r") as file_read, open(f"{PATH_TO_DATA_FOLDER}/labelled_data.csv", "w") as file_write:
         for line in file_read:
             x, y = [float(v.strip()) for v in line.split(",")] # Get values for x and y as floats from the line
             label = get_classification_label_for_point((x, y))
@@ -24,3 +28,29 @@ def classify_unlabelled_data() -> None:
 
 
 classify_unlabelled_data()
+
+def plot_line_and_classified_data_points():
+    red_points = []
+    blue_points = []
+
+    with open(f"{PATH_TO_DATA_FOLDER}/unlabelled_data.csv", "r") as file_read:
+        for line in file_read:
+            x, y = [float(v.strip()) for v in line.split(",")] # Get values for x and y as floats from the line
+            p = (x, y)
+            label = get_classification_label_for_point(p)
+
+            if label == 1:
+                blue_points.append(p)
+            else:
+                red_points.append(p)
+
+    fig, ax = plt.figure(dpi=100), plt.axes()
+    X = np.linspace(-6, 6)
+
+    ax.plot(X, [calculate_y_on_separator(x) for x in X])
+    ax.scatter([p[0] for p in red_points], [p[1] for p in red_points], alpha=0.6, c="red")
+    ax.scatter([p[0] for p in blue_points], [p[1] for p in blue_points], alpha=0.6, c="blue")
+    plt.show()
+
+
+plot_line_and_classified_data_points()
