2329f696f2d776c8ac1dcd439320b83602b20443
Philip Langpohl <philip.langpohl@outlook.com>
2024-10-16 12:08:52 +0200

    feat(lab3): moved and refactored code, and added text to report

--- Labs/Lab3/lab3.py
+++ Labs/Lab3/lab3.py
@@ -1,52 +1,63 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from lab3_utils import calculate_y_on_separator
+from typing import Callable
+from lab3_utils import calculate_y_on_separator, get_classification_label_for_point
 
-# Constants
+# Constants (Global variables that should not be changed)
 PATH_TO_DATA_FOLDER = "Labs/Lab3/Data"
 
 
-def get_classification_label_for_point(point: tuple) -> int:
-    x, y = point
-
-    # Returns 1 if the y-value of the point is greater than the y-value of the separating line at the same x-coordinate, meaning the point is above the line. Otherwise, returns 0.
-    return 1 if y > calculate_y_on_separator(x) else 0
+# The function here can be changed if points should be classified and plotted with other function
+function_for_classification = calculate_y_on_separator
 
 
-def classify_unlabelled_data() -> None:
-    # Open both the file with unlabelled data, and the new file that will contain labelled data
+def classify_unlabelled_data(f: Callable) -> None:
+    # Open both the file with unlabelled data, and the new file that will contain labelled data.
+    # Both opens here in order to only use one loop, both for reading and writing.
     with open(f"{PATH_TO_DATA_FOLDER}/unlabelled_data.csv", "r") as file_read, open(f"{PATH_TO_DATA_FOLDER}/labelled_data.csv", "w") as file_write:
         for line in file_read:
             x, y = [float(v.strip()) for v in line.split(",")] # Get values for x and y as floats from the line
-            label = get_classification_label_for_point((x, y))
+            label = get_classification_label_for_point((x, y), f)
 
             file_write.write(f"{x},{y},{label}\n") # Write the line to the new file, including the classified label
 
 
-classify_unlabelled_data()
-
-def plot_line_and_classified_data_points():
+def plot_line_and_classified_data_points(f: Callable) -> None:
     red_points = []
     blue_points = []
 
     with open(f"{PATH_TO_DATA_FOLDER}/unlabelled_data.csv", "r") as file_read:
         for line in file_read:
             x, y = [float(v.strip()) for v in line.split(",")] # Get values for x and y as floats from the line
-            p = (x, y)
-            label = get_classification_label_for_point(p)
+            point = (x, y)
+            label = get_classification_label_for_point(point, f)
 
+            # Seperate the points by the label to two different lists
             if label == 1:
-                blue_points.append(p)
+                blue_points.append(point)
             else:
-                red_points.append(p)
+                red_points.append(point)
+
 
     fig, ax = plt.figure(dpi=100), plt.axes()
+
     X = np.linspace(-6, 6)
 
-    ax.plot(X, [calculate_y_on_separator(x) for x in X])
+    # Plot the line in the graph
+    ax.plot(X, [f(x) for x in X])
+
+    # Scatter the classified points with color differenting the classes
     ax.scatter([p[0] for p in red_points], [p[1] for p in red_points], alpha=0.6, c="red")
     ax.scatter([p[0] for p in blue_points], [p[1] for p in blue_points], alpha=0.6, c="blue")
+    
+    ax.set_xlim((-6, 6))
+    ax.set_ylim((-6, 6))
+
     plt.show()
 
 
-plot_line_and_classified_data_points()
+# Call the function that will read unlabelled data, and write the labelled data to labelled_data.csv
+classify_unlabelled_data(function_for_classification)
+
+# Call the function that will read unlabelled data, and plot the line and classified points in a graph
+plot_line_and_classified_data_points(function_for_classification)
--- Labs/Lab3/lab3_utils.py
+++ Labs/Lab3/lab3_utils.py
@@ -1,80 +1,25 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from typing import Callable
 
 def calculate_y_on_separator(x: float) -> float:
-    # The line equation developed in the preparatory assignment, to seperate points evenly
+    # The line equation developed in the preparatory assignment, to seperate points evenly.
     # Returns the y-value on the separating line for a given x-value.
     return -0.725 * x + 0.475
 
 
-def f(x):
+def f(x: float) -> float:
     return -0.489*x
 
 
-def g(x):
+def g(x: float) -> float:
     return -2*x + 0.16
 
 
-def h(x):
+def h(x: float) -> float:
     return 800*x - 120
 
 
-def classify_data(points, function):
-    red_points = []
-    blue_points = []
+def get_classification_label_for_point(point: tuple, f: Callable) -> int:
+    x, y = point
 
-    for point in points:
-        x, y = point
-
-        # is_positive_x = function(x+1) > function(x)
-
-        # if is_positive_x:
-        #     label = 0 if y > function(x) else 1
-        # else: 
-        #     label = 1 if y > function(x) else 0
-
-        label = 1 if y > function(x) else 0
-
-        if label == 1:
-            blue_points.append(point)
-        else:
-            red_points.append(point)
-
-    return red_points, blue_points
-
-
-def scatter_classified_point_and_line_for_function(points, f, title=None):
-    red, blue = classify_data(points, f)
-
-    min_axis = -6
-    max_axis = 6
-
-    X = np.linspace(min_axis, max_axis)
-
-    fig, ax = plt.figure(dpi=100), plt.axes()
-
-    if title != None:
-        ax.set(title=title)
-        
-    ax.set_xlim((min_axis, max_axis))
-    ax.set_ylim((min_axis, max_axis))
-
-    ax.plot(X, [f(x) for x in X])
-    ax.scatter([p[0] for p in red], [p[1] for p in red], alpha=0.6, c="red")
-    ax.scatter([p[0] for p in blue], [p[1] for p in blue], alpha=0.6, c="blue")
-    plt.show()
-
-    print(f"Antal röda: {len(red)}")
-    print(f"Antal blåa: {len(blue)}")
-
-
-def get_unlabelled_points():
-    points = []
-
-    with open("Data/unlabelled_data.csv", "r") as file:
-        for line in file:
-            x, y = [float(v.strip()) for v in line.split(",")]
-            point = (x, y)
-            points.append(point)
-    
-    return points
+    # Returns 1 if the y-value of the point is greater than the y-value of the separating line at the same x-coordinate, meaning the point is above the line. Otherwise, returns 0.
+    return 1 if y > f(x) else 0
